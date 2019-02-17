293cfddbebe83ccc816b114a9425780b6c891ed5
Vijay Bheda <bhedavijay489@gmail.com>
2019-02-17 19:42:02 +0530

    updated view for FS using com.google.android.material.textfieldTextInputLayout

--- app/src/main/java/com/vijaybheda/admindatahandler/NewDoctor.java
+++ app/src/main/java/com/vijaybheda/admindatahandler/NewDoctor.java
@@ -1,20 +1,25 @@
 package com.vijaybheda.admindatahandler;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NewDoctor extends AppCompatActivity {
+public class NewDoctor extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText editTextName;
+    private EditText editTextName, editTextdob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,9 @@ public class NewDoctor extends AppCompatActivity {
         setContentView(R.layout.activity_new_doctor);
         setTitle("Add Doctor Details");
 
-        editTextName = findViewById(R.id.edit_text_doctorname);
-
+        editTextName = findViewById(R.id.edit_text_doctorfname);
+        editTextdob = findViewById(R.id.edit_text_drdob);
+        editTextdob.setOnClickListener(this);
     }
 
     @Override
@@ -65,5 +71,34 @@ public class NewDoctor extends AppCompatActivity {
         doctorbookRef.add(new Doctor(name, doctorcode));
         Toast.makeText(this, "Doctor added", Toast.LENGTH_SHORT).show();
         finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+        editTextdob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // DOB 
+                pickDate();
+            }
+        });
+    }
+
+    private void pickDate() {
+        int date, month, year;
+        final Calendar calendar = Calendar.getInstance();
+        date = calendar.get(Calendar.DAY_OF_MONTH);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                editTextdob.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+            }
+        }, date, month, year);
+        datePickerDialog.show();
+
     }
 }
